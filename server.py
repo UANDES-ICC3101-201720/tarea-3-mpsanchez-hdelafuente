c77b5eb2fe1acb3d25406804ca42950de9ca5671
Maria Pia Sanchez <mapisanchezg@gmail.com>
2018-11-25 17:48:02 -0300

    Server: Fix socket error

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from threading import Thread
 import socket  # Import socket module
 import json
 
+
 def Send_File(file, c):
     c.send("[Server] Sending".encode())
     f = open(file, 'rb')
@@ -19,39 +20,44 @@ def Send_File(file, c):
     c.send("[Server] Done".encode())
     return None
 
+
 def Read_Config():
     with open("config.json", "r") as config:
-    	json_data = json.loads(config.readline())
+        json_data = json.loads(config.readline())
     config.close()
     return json_data
-   
+
+
 def Update_Config(msg, ip):
     # msg seria el archivo de texto con todos los archivos que contiene la carpeta del cliente que realiza un update
     with open("config.json", "w") as config:
-    	try:
-    		json_data = json.loads(config.readline())
-    	except Exception as e:
-    		json_data = {}
-    	msg_data = json.loads(msg)
-    	for file in msg_data:
-    		for value in msg_data[file]:
-    			if value not in json_data.keys():
-    				json_data[value] = []
-    			if ip not in json_data[value]:
-    				json_data[value].append(ip)
-    	json.dump(json_data, config)
+        try:
+            json_data = json.loads(config.readline())
+        except Exception as e:
+            json_data = {}
+        msg_data = json.loads(msg)
+        for file in msg_data:
+            for value in msg_data[file]:
+                if value not in json_data.keys():
+                    json_data[value] = []
+                if ip not in json_data[value]:
+                    json_data[value].append(ip)
+        json.dump(json_data, config)
     config.close()
     return None
 
+
 def Search_File(file_name, config):
-	clients = {}
-	for file in config.keys():
-		if file_name in file:
-			clients[file] = config[file]
-	return clients
+    clients = {}
+    for file in config.keys():
+        if file_name in file:
+            clients[file] = config[file]
+    return clients
+
 
 def main():
     start_server()
+
 
 def start_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
@@ -81,38 +87,37 @@ def start_server():
             print("Thread did not start.")
             traceback.print_exc()
 
-        c.close()
-
     s.close()
 
 
 def client_thread(c, ip):
     while True:
-	    c.send('[Server] Thank you for connecting'.encode())
-	    update_msg = c.recv(1024).decode()
-	    print("client: " + update_msg)
-	    if "Update" in update_msg:
-	    	Update_Config(c.recv(1024).decode(), addr[0])
-	    
-	    exit = False
-	    while not exit:
-	    	config = Read_Config()
-	    	client_msg = c.recv(1024).decode()
-	    	if "Exit" in client_msg:
-	    		print("Ok")
-	    		exit = True
-	    		break
-	    	client_msg = json.loads(client_msg)
-	    	if client_msg["action"] == 1:
-	    		search = Search_File(client_msg["keyword"], config)
-	    		c.send(json.dumps(search).encode())
-	    	elif client_msg["action"] == 2:
-	    		print("action2")
-	    	elif client_msg["action"] == 3:
-	    		print("action 3")
-	    	else:
-	    		print("Unkown option")
-	    		continue
+        c.send('[Server] Thank you for connecting'.encode())
+        update_msg = c.recv(1024).decode()
+        print("client: " + update_msg)
+        if "Update" in update_msg:
+            Update_Config(c.recv(1024).decode(), ip)
+
+        exit = False
+        while not exit:
+            config = Read_Config()
+            client_msg = c.recv(1024).decode()
+            if "Exit" in client_msg:
+                print("Ok")
+                exit = True
+                break
+            client_msg = json.loads(client_msg)
+            if client_msg["action"] == 1:
+                search = Search_File(client_msg["keyword"], config)
+                c.send(json.dumps(search).encode())
+            elif client_msg["action"] == 2:
+                print("action2")
+            elif client_msg["action"] == 3:
+                print("action 3")
+            else:
+                print("Unkown option")
+                continue
+    c.close()
 
 
 if __name__ == "__main__":
